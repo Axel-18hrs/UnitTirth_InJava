0e385ea02c2c226ff905c8985087313f697dbb57
Axel-18hrs <Axel.18hrs@gmail.com>
2024-02-27 18:24:38 -0600

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+# appTextFiles_InJava
---
+++ UnitTirth_InJava/src/main/java/com/mycompany/unittirth_injava/UnitTirth_InJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unittirth_injava;
+import formularios.Frm_app;
+/**
+ *
+ * @author axel1
+ */
+public class UnitTirth_InJava {
+
+    public static void main(String[] args) {
+        new Frm_app().setVisible(true);
+    }
+    
+}
